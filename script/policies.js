adf222b8ac197a99b071ed194971cefc80454ba1
Arnav Gupta <arnav090404@Gmail.com>
2024-02-09 02:54:19 +0530

    made changes

--- script/policies.js
+++ script/policies.js
@@ -1,6 +1,17 @@
+function showAllHeads(){
+    if(document.getElementById("mainHeads").style.display=='none'){
+        document.getElementById("mainHeads").style.display='block';
+    }
+    else{  
+        document.getElementById("mainHeads").style.display='none';
+    }
+}
+
+
+
 function catEduTxt() {
-    
     if(document.getElementById("Nat").innerHTML==""){
+        document.getElementById("specHeads").style.display='block';
         document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
         document.getElementById("eduNat").style.display = 'block';
         document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
@@ -9,6 +20,7 @@ function catEduTxt() {
         document.getElementById("eduNgo").style.display = 'block';
     }
     else{
+        // document.getElementById("specHeads").style.display='none';
         document.getElementById("Nat").innerHTML = "";
         document.getElementById("Stat").innerHTML = "";
         document.getElementById("ngo").innerHTML = "";
@@ -17,35 +29,37 @@ function catEduTxt() {
 
 function catHealthTxt() {
 
-    if(document.getElementById("Nat").innerHTML==""){
-        document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
-        document.getElementById("eduNat").style.display = 'block';
-        document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
-        document.getElementById("eduState").style.display = 'block';
-        document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
-        document.getElementById("eduNgo").style.display = 'block';
-    }
-    else{
-        document.getElementById("Nat").innerHTML = "";
-        document.getElementById("Stat").innerHTML = "";
-        document.getElementById("ngo").innerHTML = ""; 
-    }
+    // if(document.getElementById("Nat").innerHTML==""){
+    //     document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
+    //     document.getElementById("eduNat").style.display = 'block';
+    //     document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
+    //     document.getElementById("eduState").style.display = 'block';
+    //     document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
+    //     document.getElementById("eduNgo").style.display = 'block';
+    // }
+    // else{
+    //     document.getElementById("Nat").innerHTML = "";
+    //     document.getElementById("Stat").innerHTML = "";
+    //     document.getElementById("ngo").innerHTML = ""; 
+    // }
+    hideAllTxt();
 }
 
 function catNutriTxt() {
-    if(document.getElementById("Nat").innerHTML==""){
-        document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
-        document.getElementById("eduNat").style.display = 'block';
-        document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
-        document.getElementById("eduState").style.display = 'block';
-        document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
-        document.getElementById("eduNgo").style.display = 'block';
-    }
-    else{
-        document.getElementById("Nat").innerHTML = "";
-        document.getElementById("Stat").innerHTML = "";
-        document.getElementById("ngo").innerHTML = ""; 
-    }
+    // if(document.getElementById("Nat").innerHTML==""){
+    //     document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
+    //     document.getElementById("eduNat").style.display = 'block';
+    //     document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
+    //     document.getElementById("eduState").style.display = 'block';
+    //     document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
+    //     document.getElementById("eduNgo").style.display = 'block';
+    // }
+    // else{
+    //     document.getElementById("Nat").innerHTML = "";
+    //     document.getElementById("Stat").innerHTML = "";
+    //     document.getElementById("ngo").innerHTML = ""; 
+    // }
+    hideAllTxt();
 }
 
 function catDevTxt() {
@@ -68,17 +82,17 @@ function showStatTxt(ref){
     if(ref.selectedIndex==0){
         clearSelectTxt();
     }
+    else if(ref.selectedIndex==20){
+        clearSelectTxt();
+        document.getElementById("statTxt").innerHTML=document.getElementById("PunjabTXT").innerHTML;
+    }
     else if(ref.selectedIndex==1){
         clearSelectTxt();
-        document.getElementById("statTxt").innerHTML=document.getElementById("DelhiTXT").innerHTML;
-    }
-    else if(ref.selectedIndex==2){
-        clearSelectTxt();
-        document.getElementById("statTxt").innerHTML=document.getElementById("DelhiTXT").innerHTML;
+        document.getElementById("statTxt").innerHTML="Policies in Andhra Pradesh";
     }
     else if(ref.selectedIndex==3){
         clearSelectTxt();
-        document.getElementById("statTxt").innerHTML=document.getElementById("DelhiTXT").innerHTML;
+        document.getElementById("statTxt").innerHTML=document.getElementById("PunjabTXT").innerHTML;
     }
     else{
         document.getElementById("statTxt").innerHTML="";
