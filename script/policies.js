689fbc8160cbd9ca53c30c89a077aa32ccccee97
Arnav Gupta <arnav090404@Gmail.com>
2024-02-08 21:50:15 +0530

    policy updated

--- script/policies.js
+++ script/policies.js
@@ -1,3 +1,97 @@
-function catTxt(){
-    document.getElementById("catTxt").innerHTML="helloo";
+function catEduTxt() {
+    
+    if(document.getElementById("Nat").innerHTML==""){
+        document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
+        document.getElementById("eduNat").style.display = 'block';
+        document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
+        document.getElementById("eduState").style.display = 'block';
+        document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
+        document.getElementById("eduNgo").style.display = 'block';
+    }
+    else{
+        document.getElementById("Nat").innerHTML = "";
+        document.getElementById("Stat").innerHTML = "";
+        document.getElementById("ngo").innerHTML = "";
+    }
+}
+
+function catHealthTxt() {
+
+    if(document.getElementById("Nat").innerHTML==""){
+        document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
+        document.getElementById("eduNat").style.display = 'block';
+        document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
+        document.getElementById("eduState").style.display = 'block';
+        document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
+        document.getElementById("eduNgo").style.display = 'block';
+    }
+    else{
+        document.getElementById("Nat").innerHTML = "";
+        document.getElementById("Stat").innerHTML = "";
+        document.getElementById("ngo").innerHTML = ""; 
+    }
+}
+
+function catNutriTxt() {
+    if(document.getElementById("Nat").innerHTML==""){
+        document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
+        document.getElementById("eduNat").style.display = 'block';
+        document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
+        document.getElementById("eduState").style.display = 'block';
+        document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
+        document.getElementById("eduNgo").style.display = 'block';
+    }
+    else{
+        document.getElementById("Nat").innerHTML = "";
+        document.getElementById("Stat").innerHTML = "";
+        document.getElementById("ngo").innerHTML = ""; 
+    }
+}
+
+function catDevTxt() {
+    if(document.getElementById("Nat").innerHTML==""){
+        document.getElementById("Nat").innerHTML = document.getElementById("eduNat").innerHTML;
+        document.getElementById("eduNat").style.display = 'block';
+        document.getElementById("Stat").innerHTML = document.getElementById("eduState").innerHTML;
+        document.getElementById("eduState").style.display = 'block';
+        document.getElementById("ngo").innerHTML = document.getElementById("eduNgo").innerHTML; 
+        document.getElementById("eduNgo").style.display = 'block';
+    }
+    else{
+        document.getElementById("Nat").innerHTML = "";
+        document.getElementById("Stat").innerHTML = "";
+        document.getElementById("ngo").innerHTML = ""; 
+    }
+}
+
+function showStatTxt(ref){
+    if(ref.selectedIndex==0){
+        clearSelectTxt();
+    }
+    else if(ref.selectedIndex==1){
+        clearSelectTxt();
+        document.getElementById("statTxt").innerHTML=document.getElementById("DelhiTXT").innerHTML;
+    }
+    else if(ref.selectedIndex==2){
+        clearSelectTxt();
+        document.getElementById("statTxt").innerHTML=document.getElementById("DelhiTXT").innerHTML;
+    }
+    else if(ref.selectedIndex==3){
+        clearSelectTxt();
+        document.getElementById("statTxt").innerHTML=document.getElementById("DelhiTXT").innerHTML;
+    }
+    else{
+        document.getElementById("statTxt").innerHTML="";
+    }
+}
+
+function clearSelectTxt(){
+    document.getElementById("statTxt").innerHTML="";
+}
+
+
+function hideAllTxt(){
+    document.getElementById("Nat").innerHTML = "";
+    document.getElementById("Stat").innerHTML = "";
+    document.getElementById("ngo").innerHTML = "";
 }
